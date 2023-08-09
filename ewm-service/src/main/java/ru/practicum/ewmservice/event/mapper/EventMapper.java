b0308e9b9718c2feb2f843cf01aff3167480ddd0
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-08-09 22:22:24 +0300

    diplom_ewm_3

--- ewm-service/src/main/java/ru/practicum/ewmservice/event/mapper/EventMapper.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/event/mapper/EventMapper.java
@@ -1,7 +1,10 @@
 package ru.practicum.ewmservice.event.mapper;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
+import org.modelmapper.ModelMapper;
+import ru.practicum.ewmservice.category.model.Category;
 import ru.practicum.ewmservice.event.dto.EventDto;
 import ru.practicum.ewmservice.event.model.Event;
 import ru.practicum.ewmservice.event.model.Location;
@@ -11,8 +14,9 @@ import java.time.LocalDateTime;
 
 
 @UtilityClass
-public class EventMapper {
 
+public class EventMapper {
+private final   ModelMapper mapper = new ModelMapper();
     public static Event toEntity(EventDto dto) {
 
         return Event.builder()
@@ -26,7 +30,10 @@ public class EventMapper {
                 .requestModeration(dto.isRequestModeration())
                 .state(State.PENDING)
                 .title(dto.getTitle())
+                .requestModeration(true)
+                .publishedOn(LocalDateTime.now())
                 .request(dto.getRequestId())
+//                    .category(new Category(15L,"without category"))
                 .build();
 //    public static EndpointHitDto toDto(EndpointHit entity) {
 //        return EndpointHitDto.builder()
