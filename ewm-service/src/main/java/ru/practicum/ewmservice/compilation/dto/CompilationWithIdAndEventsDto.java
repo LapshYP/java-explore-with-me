e90cdb990619cb8a89af8cc8a59fddf60a7308f6
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-08-18 14:19:25 +0300

    diplom_ewm_13_to_review

--- ewm-service/src/main/java/ru/practicum/ewmservice/compilation/dto/CompilationWithIdAndEventsDto.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/compilation/dto/CompilationWithIdAndEventsDto.java
@@ -2,9 +2,7 @@ package ru.practicum.ewmservice.compilation.dto;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.ewmservice.event.dto.EventDto;
 import ru.practicum.ewmservice.event.dto.EventShortDto;
-
 
 import java.util.Set;
 
@@ -20,5 +18,5 @@ public class CompilationWithIdAndEventsDto {
     Long id;
     String title;
     Boolean pinned;
-     Set<EventShortDto> events;
+    Set<EventShortDto> events;
 }
