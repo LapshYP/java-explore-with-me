2abe13b323b48032b1c3047d6ecd4f1a5a7422b1
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-08-19 14:30:45 +0300

    diplom_ewm_16_to_review_yml

--- ewm-service/src/main/java/ru/practicum/ewmservice/event/repository/EventCriteriaRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/event/repository/EventCriteriaRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import ru.practicum.ewmservice.event.model.CriteriaAdmin;
-import ru.practicum.ewmservice.event.model.Event;
 import ru.practicum.ewmservice.event.model.CriteriaUser;
-
-import java.util.stream.Collectors;
+import ru.practicum.ewmservice.event.model.Event;
 
 
 @Repository
