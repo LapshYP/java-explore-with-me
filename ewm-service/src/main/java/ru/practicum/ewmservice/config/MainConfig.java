111a9e985eab89eabd91116d17dfeed002cb708f
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-08-21 17:10:27 +0300

    после проверки 2 части диплома_3

--- ewm-service/src/main/java/ru/practicum/ewmservice/config/MainConfig.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/config/MainConfig.java
@@ -1,4 +1,4 @@
-package ru.practicum.ewmservice.stats;
+package ru.practicum.ewmservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
